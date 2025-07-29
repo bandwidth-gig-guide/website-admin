a716883e9c9600d6957e9315c2d5fb28fe581ba8
MatthewLeigh <matthewcross97@gmail.com>
2025-07-30 09:33:42 +1000

    feat: redirect to /event when / is mounted

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,40 +1,12 @@
-// React / Next
-import React, {useState, useEffect} from "react"
-import Head from 'next/head';
-
-// Utils
-import { fetchIds } from '../util/fetchIds'
-
+import { useEffect} from "react"
+import { useRouter } from 'next/router';
 
 const Home = () => {
-  const [artists, setArtists] = useState<uuid[]>([]);
-  const [events, setEvents] = useState<uuid[]>([]);
-  const [venues, setVenues] = useState<uuid[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
-    fetchIds('artist', setArtists);
-    fetchIds('event', setEvents);
-    fetchIds('venue', setVenues);
+    router.replace('/event');
   }, []);
-
-
-  return (
-    <>
-      <Head>
-        <title>Bandwidth</title>
-        <meta name="description" content="Welcome to my website" />
-      </Head>
-
-      <h1>Homepage</h1>
-      <h2>Events</h2>
-      {events}
-      <h2>Artists</h2>
-      {artists}
-      <h2>Venues</h2>
-      {venues}
-
-    </>
-  );
 };
 
 export default Home;
