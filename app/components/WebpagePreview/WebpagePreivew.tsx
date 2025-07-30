6c8234a4fbd8c736b9a266f11110b4c17f645403
MatthewLeigh <matthewcross97@gmail.com>
2025-07-30 21:07:56 +1000

    feat: add webpage preview component

---
+++ app/components/WebpagePreview/WebpagePreivew.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './WebpagePreview.module.css'
+
+interface Props {
+  url: string;
+  title?: string;
+}
+
+const WebPageEmbed: React.FC<Props> = ({ url, title = 'Embedded Page' }) => {
+  return (
+    <div className={styles.wrapper}>
+      <iframe
+        src={url}
+        title={title}
+      />
+    </div>
+  );
+};
+
+export default WebPageEmbed;
