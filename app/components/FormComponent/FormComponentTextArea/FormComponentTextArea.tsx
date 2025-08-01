2b240eb9f3c9d8143dd1094b2cd372b60dd8570b
MatthewLeigh <matthewcross97@gmail.com>
2025-08-01 11:34:26 +1000

    feat: add word count to description

--- app/components/FormComponent/FormComponentTextArea/FormComponentTextArea.tsx
+++ app/components/FormComponent/FormComponentTextArea/FormComponentTextArea.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from './FormComponentTextArea.module.css';
+import { DESCRIPTION_MIN, DESCRIPTION_MAX } from "../../../constants/minMaxValues";
 
 interface Props {
   label: string;
@@ -7,6 +9,7 @@ interface Props {
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   required?: boolean;
   rows?: number;
+  isDescription?: boolean;
 }
 
 const FormComponentTextArea: React.FC<Props> = ({
@@ -15,19 +18,50 @@ const FormComponentTextArea: React.FC<Props> = ({
   value,
   onChange,
   required = true,
-  rows = 20
-}) => (
-  <div>
-    <label htmlFor={name}>{label}</label>
-    <textarea
-      id={name}
-      name={name}
-      value={value}
-      onChange={onChange}
-      required={required}
-      rows={rows}
-    />
-  </div>
-);
+  rows = 20,
+  isDescription = true
+}) => {
+
+  const [isAppropriateLength, setIsAppropriateLength] = useState<boolean>(false);
+  const [wordCount, setWordCount] = useState<number>(0);
+
+  useEffect(() => {
+    if (!isDescription) {
+      setIsAppropriateLength(true);
+    }
+  }, [isDescription])
+
+  useEffect(() => {
+    setWordCount(value.trim().split(/\s+/).filter(Boolean).length);
+    if (isDescription) {
+      setIsAppropriateLength(
+        wordCount >= DESCRIPTION_MIN &&
+        wordCount <= DESCRIPTION_MAX
+      )
+    }
+  }, [value])
+
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={name}>{label}</label>
+      <textarea
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        required={required}
+        rows={rows}
+      />
+      <div className={styles.wordCount}>
+        <span className={!isAppropriateLength ? styles.warning : ''}>
+          {wordCount}
+        </span>
+        {isDescription && (
+          <span>&nbsp;| Target: {DESCRIPTION_MIN} - {DESCRIPTION_MAX}</span>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default FormComponentTextArea;
---
+++ app/constants/minMaxValues.ts
@@ -0,0 +1,2 @@
+export const DESCRIPTION_MIN = 150;
+export const DESCRIPTION_MAX = 500;
