1913d9b727de1e3bc9fe64749ba9d657cbf252c6
MatthewLeigh <matthewcross97@gmail.com>
2025-08-04 15:48:07 +1000

    fix: define prev type

--- app/components/FormComponent/FormComponentOpeningHours/FormComponentOpeningHours.tsx
+++ app/components/FormComponent/FormComponentOpeningHours/FormComponentOpeningHours.tsx
@@ -40,6 +40,7 @@ function formatTime(time: string) {
 }
 
 const FormComponentOpeningHours = ({ record, setRecord }: Props) => {
+
   useEffect(() => {
     if (!record.openingHours) {
       setRecord(prev => ({
@@ -50,7 +51,7 @@ const FormComponentOpeningHours = ({ record, setRecord }: Props) => {
   }, []);
 
   const handleTimeChange = (key: string, value: string) => {
-    setRecord(prev => ({
+    setRecord((prev: any) => ({
       ...prev,
       openingHours: {
         ...prev.openingHours,
@@ -60,7 +61,7 @@ const FormComponentOpeningHours = ({ record, setRecord }: Props) => {
   };
 
   const handleClosed = (openKey: string, closeKey: string) => {
-    setRecord(prev => ({
+    setRecord((prev: any) => ({
       ...prev,
       openingHours: {
         ...prev.openingHours,
@@ -71,7 +72,7 @@ const FormComponentOpeningHours = ({ record, setRecord }: Props) => {
   };
 
   const handleOpenTilLate = (closeKey: string) => {
-    setRecord(prev => ({
+    setRecord((prev: any) => ({
       ...prev,
       openingHours: {
         ...prev.openingHours,
