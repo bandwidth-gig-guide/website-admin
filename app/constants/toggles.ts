8d20ef5c65d57ee333c1f96d66ec57e02ea916a8
MatthewLeigh <matthewcross97@gmail.com>
2025-08-15 13:44:19 +1000

    refactor: move toggles to dedicated file

---
+++ app/constants/toggles.ts
@@ -0,0 +1,5 @@
+export const TOGGLES = [
+  { id: "json", label: "JSON", src: "/json-object.svg" },
+  { id: "og-post", label: "Original Post", src: "/original-post.svg" },
+  { id: "ticket-sale", label: "Ticket Sale", src: "/ticket-sale.svg" },
+]
