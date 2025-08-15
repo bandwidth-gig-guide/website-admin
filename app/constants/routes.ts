d93230dcbf8128b5d329a8f1dbef2ada8b5c3979
MatthewLeigh <matthewcross97@gmail.com>
2025-08-15 13:44:09 +1000

    refactor: move routes to dedicated file

---
+++ app/constants/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = [
+  { label: "Events", type: 'event', href: "/event", toggles: ["json", "og-post", "ticket-sale"] },
+  { label: "Artists", type: 'artist', href: "/artist", toggles: ["json"] },
+  { label: "Venues", type: 'venue', href: "/venue", toggles: ["json"] },
+  { label: "Images", type: 'image', href: "/images", toggles: ["json"] },
+  { label: "Featured", type: 'feature', href: "/features", toggles: ["json"] },
+];
