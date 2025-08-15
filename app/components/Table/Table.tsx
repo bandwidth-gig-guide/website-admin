76a39e5e6344bf8d8920271f3713b7534ed65c32
MatthewLeigh <matthewcross97@gmail.com>
2025-08-15 20:36:55 +1000

    feat: add border to table

--- app/components/Table/Table.tsx
+++ app/components/Table/Table.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { PageType } from '../../types/enums/PageType';
 
+// Styles
+import styles from "./Table.module.css"
+
 import TableArtist from './TableArtist/TableArtist';
 import TableEvent from './TableEvent/TableEvent';
 import TableVenue from './TableVenue/TableVenue';
@@ -11,13 +14,13 @@ interface Props {
 }
 
 const Table: React.FC<Props> = ({ids, pageType}) => {
-  switch (pageType) {
-    case PageType.Artist: return <TableArtist ids={ids} />;
-    case PageType.Event: return <TableEvent ids={ids} />;
-    case PageType.Venue: return <TableVenue ids={ids} />;
-    default: break;
-  }
-
+  return (
+    <div className={styles.wrapper}>
+      {pageType === PageType.Artist && <TableArtist ids={ids} />}
+      {pageType === PageType.Event && <TableEvent ids={ids} />}
+      {pageType === PageType.Venue && <TableVenue ids={ids} />}
+    </div>
+  )
 }
 
 export default Table
