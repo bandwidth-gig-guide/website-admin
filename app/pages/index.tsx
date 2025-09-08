05712ad729f4daf4f8444012e6bbe7074d0c7809
MatthewLeigh <matthewcross97@gmail.com>
2025-09-08 21:40:00 +1000

    feat: homepage displays buttons to create new records

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,25 @@
-import { useEffect} from "react"
+import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { ROUTES_NEW_PAGES } from "../constants/routes";
 
 const Home = () => {
   const router = useRouter();
 
-  useEffect(() => {
-    router.replace('/event');
-  }, []);
+  return (
+    <>
+      <Head>
+        <title>Bandwidth Admin</title>
+        <meta name="description" content="" />
+      </Head>
+
+      {ROUTES_NEW_PAGES.map((route, index) => (
+        <button key={index} onClick={() => router.push(route.href)}>
+          {route.label}
+        </button>
+      ))}
+
+    </>
+  )
 };
 
 export default Home;
