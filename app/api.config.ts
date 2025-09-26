c7bf21f9dc635b80cea191796b9c4111bb0a07e4
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-09-26 18:59:24 +1000

    fix: use https in api url

--- app/api.config.ts
+++ app/api.config.ts
@@ -1,3 +1,3 @@
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://admin.bandwidth.melbourne/admin";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://admin.bandwidth.melbourne/admin";
 
 export default apiUrl;
