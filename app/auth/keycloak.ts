f7ac630f884849585c12a60588e3317c06746cc4
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-09-26 18:05:31 +1000

    test(temp): hardcode prod keycloak object config

--- app/auth/keycloak.ts
+++ app/auth/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 export const keycloak = new Keycloak({
-  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL || "http://auth.localhost",
-  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM || "bandwidth.dev",
-  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID || "admin.localhost",
+  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL || "https://auth.bandwidth.melbourne",
+  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM || "bandwidth",
+  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID || "admin.bandwidth.melbourne",
 });
